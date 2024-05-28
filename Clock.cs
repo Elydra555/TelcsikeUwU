739169d57dd0686bfdffc6133eac85fd19128bda
Elydra5 <kliberbenedek@gmail.com>
2024-05-28 18:10:40 +0200

    Add project files.

---
+++ Clock.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Timers;
+
+namespace TelcsikeUwU
+{
+    public class ClockViewModel : INotifyPropertyChanged
+    {
+        private System.Timers.Timer _timer;
+        private DateTime _currentTime;
+
+        public DateTime CurrentTime
+        {
+            get { return _currentTime; }
+            set
+            {
+                _currentTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ClockViewModel()
+        {
+            _timer = new System.Timers.Timer(1000); // Frissítés minden másodpercben
+            _timer.Elapsed += OnTimerElapsed;
+            _timer.Start();
+            CurrentTime = DateTime.Now;
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            CurrentTime = DateTime.Now;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TelcsikeUwU
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new ClockViewModel();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
